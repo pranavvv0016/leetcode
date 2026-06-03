a809a0b25fa0388bc8289f554dc1bfe033e1e150
pranavvv0016 <pranavnarsingoju07@gmail.com>
2026-06-03 11:51:42 +0530

    Time: 8 ms (90.45%), Space: 48.8 MB (82.22%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,39 +1,23 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int max = 0;
-        for (int[] inte : intervals) {
-            max = Math.max(max, inte[0]);
-        }
-        int[] map = new int[max + 1];
-        for (int[] inte : intervals) {
-            int start = inte[0];
-            int end = inte[1];
-            map[start] = Math.max(end + 1, map[start]);
-        }
-        int mergeCount = 0;
-        int start = -1;
-        int end = -1;
-        for (int i = 0; i < map.length; i++) {
-            if (map[i] != 0) {
-                if (start == -1) {
-                    start = i;
-                }
-
-                end = Math.max(end, map[i] - 1);
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        List<int[] >res=new ArrayList<>();
+        res.add(intervals[0]);
+        int j=0;
+        for(int i=0;i<intervals.length;i++){
+            int[]inter=res.get(j);
+            if(intervals[i][0]<=inter[1]){
+                inter[1]=Math.max(inter[1],intervals[i][1]);
             }
-            if (i == end) {
-                intervals[mergeCount++] = new int[] { start, end };
-                start = -1;
-                end = -1;
+            else{
+                res.add(intervals[i]);
+                j++;
             }
         }
-        if (start != -1) {
-            intervals[mergeCount++] = new int[] { start, end };
+        int [][]arr=new int [j+1][2];
+        for(int i=0;i<j+1;i++){
+            arr[i]=res.get(i);
         }
-        int[][] res = new int[mergeCount][];
-        for (int i = 0; i < mergeCount; i++) {
-            res[i] = intervals[i];
-        }
-        return res;
+        return arr;
     }
 }
