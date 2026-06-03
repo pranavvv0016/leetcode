51cae5634f23a5e9afac8f0794887e1838cf331b
pranavvv0016 <pranavnarsingoju07@gmail.com>
2026-06-03 11:36:14 +0530

    Time: 0 ms (100%), Space: 48.8 MB (90.22%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -4,14 +4,12 @@ class Solution {
         for (int[] inte : intervals) {
             max = Math.max(max, inte[0]);
         }
-
         int[] map = new int[max + 1];
         for (int[] inte : intervals) {
             int start = inte[0];
             int end = inte[1];
             map[start] = Math.max(end + 1, map[start]);
         }
-
         int mergeCount = 0;
         int start = -1;
         int end = -1;
@@ -23,24 +21,19 @@ class Solution {
 
                 end = Math.max(end, map[i] - 1);
             }
-
             if (i == end) {
                 intervals[mergeCount++] = new int[] { start, end };
                 start = -1;
                 end = -1;
             }
         }
-
         if (start != -1) {
             intervals[mergeCount++] = new int[] { start, end };
         }
-
         int[][] res = new int[mergeCount][];
-
         for (int i = 0; i < mergeCount; i++) {
             res[i] = intervals[i];
         }
-
         return res;
     }
 }
