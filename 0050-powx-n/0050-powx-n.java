ca363615f4a563db3150eabfa4b2152396dd9559
pranavvv0016 <pranavnarsingoju07@gmail.com>
2026-06-05 12:23:15 +0530

    Time: 0 ms (100%), Space: 47.8 MB (63.38%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,5 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+         if (x == 0) return 0;
+        if (n == 0) return 1;
+        double res = 1;
+        long power = Math.abs((long)n);
+        while (power > 0) {
+            if ((power & 1) == 1) {
+                res *= x;
+            }
+            x *= x;
+            power >>= 1;
+        }
+        return n >= 0 ? res : 1 / res;
     }
 }
