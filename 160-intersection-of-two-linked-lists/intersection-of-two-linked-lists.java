a12ade0b708b7c591a86f3093132dd318c8a54e6
pranavvv0016 <pranavnarsingoju07@gmail.com>
2026-06-11 20:57:26 +0530

    Time: 1 ms (99.95%) | Memory: 52.6 MB (77.68%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -13,12 +13,10 @@
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode lista = headA;
         ListNode listb = headB;
-
         while (lista != listb) {
             lista = (lista != null) ? lista.next : headB;
             listb = (listb != null) ? listb.next : headA;
         }
-
         return lista;        
     }
 }
